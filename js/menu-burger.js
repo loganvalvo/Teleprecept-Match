89254a96abb80fa18f81dd2b0b2156a5db83b1e5
Devin Dougherty <docdev9799@gmail.com>
2022-01-30 14:22:09 -0500

    project bones

---
+++ js/menu-burger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+	var x = document.getElementById("menu-toggle");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
