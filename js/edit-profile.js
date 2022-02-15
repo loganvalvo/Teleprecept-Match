ab553df4d9da329e572a32d10300a498842af4c6
cavenderj <cavenderj@duq.edu>
2022-02-15 15:18:38 -0500

    profile editing functionality
    initial push for debugging

---
+++ js/edit-profile.js
@@ -0,0 +1,9 @@
+function editFunc()
+{
+
+	var bio = document.getElementById("personal-bio").style.display = none;
+	var special = document.getElementById("personal-specialities").style.display = none;
+	var bioedit = document.getElementById("bio-edit").style.display = block;
+	var specialedit = document.getElementById("specialities-edit").style.display = block;
+
+}
